3ba0090042124ea916f0ae73da6321bd141eff2b
codedogfish <jackcool1989@outlook.com>
2015-10-13 23:17:43 +0800

    Practice on basic of FIS3

---
+++ demo-simple/fis-conf.js
@@ -0,0 +1,26 @@
+fis.match('*.{png,js,css}',{
+    release: '/static/$0'
+});
+fis.media('prod').match('*.{png,js,css}',{
+    useHash: true
+});
+// fis.match('::packager', {
+//   spriter: fis.plugin('csssprites')
+// });
+
+// fis.match('*', {
+//   useHash: false
+// });
+
+// fis.match('*.js', {
+//   optimizer: fis.plugin('uglify-js')
+// });
+
+// fis.match('*.css', {
+//   useSprite: true,
+//   optimizer: fis.plugin('clean-css')
+// });
+
+// fis.match('*.png', {
+//   optimizer: fis.plugin('png-compressor')
+// });
